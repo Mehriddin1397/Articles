d7c5a107f3298564a980c337b88804303b92b89d
Mehriddin1397 <soyibovmehriddin@gmail.com>
2025-11-13 11:15:57 +0500

    google 2

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,15 @@
 
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 
 
 //Route::get('/',[PageController::class,'index'])->name('index');
-Route::get('/contact',[PageController::class,'contact'])->name('contact');
-Route::get('/articles',[PageController::class,'articles'])->name('articles');
-Route::get('/articles_show',[PageController::class,'articles_show'])->name('articles_show');
 
 Route::get('/', [\App\Http\Controllers\Web\ScientistWebController::class, 'index'])->name('web.scientists.index');
 Route::get('/scientists/{id}', [\App\Http\Controllers\Web\ScientistWebController::class, 'show'])->name('web.scientists.show');
 Route::get('/article/{id}', [\App\Http\Controllers\Web\ArticleWebController::class, 'show'])->name('web.articles.show');
-
 
 
 
