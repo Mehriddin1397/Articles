871626d9d4df460e3bf748e756ff78ea0636e94d
Mehriddin1397 <soyibovmehriddin@gmail.com>
2025-12-16 14:21:27 +0500

    2dars5

--- public/Bot/2-dars.php
+++ public/Bot/2-dars.php
@@ -22,7 +22,7 @@ function bot($method, $params = [])
 
     $res = curl_exec($curl);
     curl_close($curl);
-    if (!curl_eror($curl)) return json_decode($res, true);
+    if (!curl_error($curl)) return json_decode($res, true);
 
 }
 dump(bot("getMe",[]));
